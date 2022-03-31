891f2ee94d94684f732e5a9be9d86a19fbf48250
DESKTOP-F26E13M\Edward <kieuminhhien@gmail.com>
2022-03-31 16:26:25 +0700

    commit: set default json info when app frist run.

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WebAPI.Controllers
 {
@@ -10,9 +6,7 @@ namespace WebAPI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            return Json(new { Version = 1.0, AppName = "WebApi" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
